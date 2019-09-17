15413db898d318941524fddf75d99684b186ed4a
斯威 <1476656257@qq.com>
2019-09-17 22:47:34 +0800

    提交代码

---
+++ 9.17/Project3/Project3/1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int year;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+			printf("%d\t", year);
+	}
+	getchar();
+	return 0;
+}
---
+++ 9.17/Project4/Project4/1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i, j;
+		for (i = 100; i <= 200; i++)
+			for(j=2;j<=i/2;j++)
+		{
+				if (i%j == 0)break;
+				if (j == i / 2)
+					printf("%d\t", i);
+		}
+		getchar();
+	return 0;
+}
