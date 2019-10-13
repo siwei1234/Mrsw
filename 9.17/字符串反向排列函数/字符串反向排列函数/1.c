38f16a6b605872ed14d465525de6b9da1a58f379
斯威 <1476656257@qq.com>
2019-10-13 19:47:53 +0800

    提交代码

--- 9.17/字符串反向排列函数/字符串反向排列函数/1.c
+++ 9.17/字符串反向排列函数/字符串反向排列函数/1.c
@@ -1,16 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<string.h>
 char reverse_string(char* string)
 {
-	if (*string != '\0')
+	char tmp;
+	tmp = *string;
+	int sz = strlen(string) - 1;
+	*string = *(string + sz);
+	*(string + sz) = '\0';
+	if (strlen(string) > 1)
 	{
-		reverse_string(string+1);
+		reverse_string(string + 1);
 	}
-	printf("%c", *string);
+	*(string + sz) = tmp;
 }
 int main()
 {
 	char a[] = "hello";
 	reverse_string(a);
+	printf("%s", a);
 	return 0;
 }
