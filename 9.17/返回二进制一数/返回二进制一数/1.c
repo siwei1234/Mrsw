98da8d6d3d6a15cb1970a647b74b81778ea6deb4
斯威 <1476656257@qq.com>
2019-10-16 21:13:01 +0800

    提交代码

---
+++ 9.17/返回二进制一数/返回二进制一数/1.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int f(int a)
+{
+	int n = 0;
+	while (a)
+	{
+		if (a % 2 == 1)
+		{
+			n++;
+		}
+		a = a / 2;
+	}
+	return n;
+}
+int main()
+{
+	int a = 0;
+	int b;
+	printf("«Î ‰»Î ˝◊÷£∫\n");
+	scanf("%d", &a);
+	b = f(a);
+	printf("%d", b);
+	return 0;
+}
