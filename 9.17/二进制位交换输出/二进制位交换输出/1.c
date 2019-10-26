35d50315361e4b365ccf25efb3429641d9b36394
斯威 <1476656257@qq.com>
2019-10-26 21:50:55 +0800

    提交代码

--- 9.17/二进制位交换输出/二进制位交换输出/1.c
+++ 9.17/二进制位交换输出/二进制位交换输出/1.c
@@ -1,34 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void f(int a)
-{
-	int i = 0;
-	int m[32] = { 0 };
-	int count = 0;
-	while(a)
-	{
-		m[i++] = a % 2;
-		a = a / 2;
-		count++;
-	}
-	printf("奇数序列\n");
-    for (i = 0; i <= count; i += 2) 
-	{
-		printf("%d ", m[i]);
-	}
-    printf("\n");
-    printf("偶数序列\n");
-    for (i = 1; i <count; i += 2) 
-    {
-		printf("%d ", m[i]);
-    }
-	printf("\n");
-}
 int main()
 {
+	int i;
 	int a = 0;
 	printf("请输入数字：\n");
 	scanf("%d", &a);
-	f(a);
+	printf("偶数位为：\n");
+	for (i = 30; i >= 0; i -= 2)
+	{
+		printf("%d", ((a >> i) & 1));
+	}
+	printf("\n奇数位为：\n");
+	for (i = 31; i > 0; i -= 2)
+	{
+		printf("%d", ((a >> i) & 1));
+	}
 	return 0;
 }
