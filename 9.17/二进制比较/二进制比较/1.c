838d312619094f357873185552774b3e746a986c
斯威 <1476656257@qq.com>
2019-10-26 21:49:35 +0800

    提交代码

--- 9.17/二进制比较/二进制比较/1.c
+++ 9.17/二进制比较/二进制比较/1.c
@@ -1,32 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int f(int a, int m[32])
-{
-	int i = 0;
-	while (a > 0)
-	{
-		m[i++] = a % 2;
-		a /= 2;
-	}
-}
 int main()
 {
-	int i;
-	int count = 0;
 	int a = 0, b = 0;
-	int m[32] = { 0 };
-	int n[32] = { 0 };
+	int i;
+	int n = 0;
 	printf("请输入两个数：\n");
 	scanf("%d%d", &a, &b);
-	f(a, m);
-	f(b, n);
 	for (i = 0; i < 32; i++)
 	{
-		if (m[i] != n[i])
+		if (((a >> i) & 1) != ((b >> i) & 1))
 		{
-			count++;
+			n++;
 		}
 	}
-	printf("%d", count);
-	return 0;
+	printf("%d", n);
 }
