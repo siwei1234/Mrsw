2978c5d943feb05becd5dd5f6f3d1e11a06b7c2b
斯威 <1476656257@qq.com>
2019-11-26 17:15:49 +0800

    提交代码

--- 9.17/file/file/1.c
+++ 9.17/file/file/1.c
@@ -32,29 +32,29 @@
 //    fclose(pin);
 //	return 0;
 //}
-int main()
-{
-	FILE* p = fopen("test.txt", "r");
-	fseek(p, 3, SEEK_SET);
-	fseek(p, 2, SEEK_CUR);
-	char ch = fgetc(p);
-	while (ch != EOF)
-	{
-		printf("%c", ch);
-		ch = fgetc(p);
-	}
-	printf("\n");
-	fseek(p, 0, SEEK_SET);
-	fseek(p, -5, SEEK_END);
-	char ch1 = fgetc(p);
-	while (ch1 != EOF)
-	{
-		printf("%c", ch1);
-		ch1 = fgetc(p);
-	}
-  fclose(p);
-	return 0;
-}
+//int main()
+//{
+//	FILE* p = fopen("test.txt", "r");
+//	fseek(p, 3, SEEK_SET);
+//	fseek(p, 2, SEEK_CUR);
+//	char ch = fgetc(p);
+//	while (ch != EOF)
+//	{
+//		printf("%c", ch);
+//		ch = fgetc(p);
+//	}
+//	printf("\n");
+//	fseek(p, 0, SEEK_SET);
+//	fseek(p, -5, SEEK_END);
+//	char ch1 = fgetc(p);
+//	while (ch1 != EOF)
+//	{
+//		printf("%c", ch1);
+//		ch1 = fgetc(p);
+//	}
+//  fclose(p);
+//	return 0;
+//}
 //int main()
 //{
 //	FILE* p = fopen("test.txt", "r");
@@ -62,3 +62,21 @@ int main()
 //	long int size = ftell(p);
 //	printf("%d", size);
 //}
+struct student
+{
+	char name[20];
+	int age;
+};
+int main()
+{
+	struct student s = { "siwei",18 };
+	FILE* p = fopen("studet.dat", 'wb');
+	if (p == NULL)
+	{
+		printf("打开文件失败\n");
+		return 0;
+	}
+	fwrite(&s, 24, 1, p);
+	fclose(p);
+	return  0;
+}
