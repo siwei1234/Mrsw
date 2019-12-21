d1e901b0ffdd963e4aa45919d5c8d4fed88fe338
斯威 <1476656257@qq.com>
2019-12-21 23:33:55 +0800

    提交代码

---
+++ 9.17/Stack/Stack/tese.c
@@ -0,0 +1,23 @@
+#include "Stack.h"
+int main()
+{
+	Stack st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+	printf("\n");
+	StackDestroy(&st);
+	return 0;
+}
