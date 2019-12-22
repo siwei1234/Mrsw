36bf334588699b33f8768560e4426381164dc0f8
斯威 <1476656257@qq.com>
2019-12-22 23:54:07 +0800

    提交代码

---
+++ 9.17/cycleQueue/cycleQueue/Queue.c
@@ -0,0 +1,84 @@
+typedef struct {
+	int* a;
+	int n;
+	int front;
+	int rear;
+} MyCircularQueue;
+
+/** Initialize your data structure here. Set the size of the queue to be k. */
+MyCircularQueue* myCircularQueueCreate(int k) {
+	MyCircularQueue* cp = (MyCircularQueue*)malloc(sizeof(MyCircularQueue)*(k + 1));
+	cp->n = k + 1;
+	cp->front = cp->rear = 0;
+	return cp;
+}
+
+/** Checks whether the circular queue is full or not. */
+int myCircularQueueIsFull(MyCircularQueue* obj) {
+	obj->rear = (obj->rear + 1) % (obj->n);
+	if (obj->rear == obj->front)
+		return 1;
+	return 0;
+}
+/** Checks whether the circular queue is empty or not. */
+int myCircularQueueIsEmpty(MyCircularQueue* obj) {
+	if (obj->front == obj->rear)
+		return 1;
+	return 0;
+}
+
+
+
+/** Insert an element into the circular queue. Return true if the operation is successful. */
+void myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+	if (!myCircularQueueIsFull(obj))
+	{
+		obj->a[obj->rear] = value;
+		obj->rear++;
+		obj->rear %= obj->n;
+	}
+}
+
+/** Delete an element from the circular queue. Return true if the operation is successful. */
+void myCircularQueueDeQueue(MyCircularQueue* obj) {
+	if (!myCircularQueueIsEmpty(obj))
+	{
+		obj->front++;
+		if (obj->front == obj->n)
+		{
+			obj->front = 0;
+		}
+	}
+}
+
+/** Get the front item from the queue. */
+int myCircularQueueFront(MyCircularQueue* obj) {
+
+}
+
+/** Get the last item from the queue. */
+int myCircularQueueRear(MyCircularQueue* obj) {
+
+}
+
+void myCircularQueueFree(MyCircularQueue* obj) {
+
+}
+
+/**
+ * Your MyCircularQueue struct will be instantiated and called as such:
+ * MyCircularQueue* obj = myCircularQueueCreate(k);
+ * bool param_1 = myCircularQueueEnQueue(obj, value);
+
+ * bool param_2 = myCircularQueueDeQueue(obj);
+
+ * int param_3 = myCircularQueueFront(obj);
+
+ * int param_4 = myCircularQueueRear(obj);
+
+ * bool param_5 = myCircularQueueIsEmpty(obj);
+
+ * bool param_6 = myCircularQueueIsFull(obj);
+
+ * myCircularQueueFree(obj);
+*/
