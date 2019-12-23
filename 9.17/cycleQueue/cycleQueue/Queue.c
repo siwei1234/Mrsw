b656b31a25e0964332d80146504612e9dc8b0c14
斯威 <1476656257@qq.com>
2019-12-23 23:18:21 +0800

    提交代码

--- 9.17/cycleQueue/cycleQueue/Queue.c
+++ 9.17/cycleQueue/cycleQueue/Queue.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include<assert.h>
+#include<malloc.h>
 typedef struct {
 	int* a;
 	int n;
@@ -53,32 +56,27 @@ void myCircularQueueDeQueue(MyCircularQueue* obj) {
 
 /** Get the front item from the queue. */
 int myCircularQueueFront(MyCircularQueue* obj) {
-
+	if (!myCircularQueueIsEmpty(obj))
+		return -1;
+	return obj->a[obj->front];
 }
 
 /** Get the last item from the queue. */
 int myCircularQueueRear(MyCircularQueue* obj) {
-
+	if (!myCircularQueueIsEmpty(obj))
+		return -1;
+	int rearNext;
+	if (obj->rear == 0)
+		rearNext = obj->n - 1;
+	return obj->a[rearNext];
 }
 
 void myCircularQueueFree(MyCircularQueue* obj) {
+	free(obj->a);
+	free(obj);
 
 }
+int main()
+{
 
-/**
- * Your MyCircularQueue struct will be instantiated and called as such:
- * MyCircularQueue* obj = myCircularQueueCreate(k);
- * bool param_1 = myCircularQueueEnQueue(obj, value);
-
- * bool param_2 = myCircularQueueDeQueue(obj);
-
- * int param_3 = myCircularQueueFront(obj);
-
- * int param_4 = myCircularQueueRear(obj);
-
- * bool param_5 = myCircularQueueIsEmpty(obj);
-
- * bool param_6 = myCircularQueueIsFull(obj);
-
- * myCircularQueueFree(obj);
-*/
+}
