de1616da7d9b2042a58edf51eabeb36fbbe30f18
斯威 <1476656257@qq.com>
2020-03-13 17:51:09 +0800

    提交代码

---
+++ 9.17/inorderTraversal/inorderTraversal/test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<malloc.h>
+struct TreeNode {
+	int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+ };
+int sum(struct TreeNode* root)
+{
+	if (root == NULL)
+		return 0;
+	return 1 + sum(root->left) + sum(root->right);
+}
+void Traversal(struct TreeNode* root, int* a, int* i)
+{
+	if (root == NULL)
+		return;
+	Traversal(root->left, a, i);
+	a[(*i)++] = root->val;
+	Traversal(root->right, a, i);
+}
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+	int size = sum(root);
+	int* ret = (int*)malloc(sizeof(int) * size);
+	*returnSize = size;
+	int i = 0;
+	Traversal(root, ret, &i);
+	return ret;
+}
+int main()
+{
+	return 0;
+}
